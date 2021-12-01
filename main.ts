2daed608e8d81add1b47d35f726567d8ff5cc672
kieuahn <73641824+kieuahn@users.noreply.github.com>
2021-12-01 16:38:55 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-basic.forever(function () {
     for (let index = 0; index < 2; index++) {
         music.playTone(233, music.beat(BeatFraction.Half))
         music.rest(music.beat(BeatFraction.Half))
@@ -50,5 +47,64 @@ basic.forever(function () {
     music.playTone(196, music.beat(BeatFraction.Half))
     music.playTone(185, music.beat(BeatFraction.Half))
     music.rest(music.beat(BeatFraction.Half))
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(233, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(233, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
     basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(233, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(196, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.playTone(233, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(196, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Eighth))
+    }
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.forever(function () {
+	
 })
